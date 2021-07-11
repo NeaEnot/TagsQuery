0bf3b2bb1c764c95f246b7b412c66dcdfd37f106
NeaEnot <oleg.kozloss@yandex.ru>
2021-07-11 10:21:38 +0400

    Добавьте файлы проекта.

---
+++ TagsQuery/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TagsQuery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
